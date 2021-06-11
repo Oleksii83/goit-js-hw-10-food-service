ed6446031b979c5048d1b3128b3ad902fd87aa64
Oleksii83 <zaaytav@gmail.com>
2021-06-11 20:47:59 +0300

    Щаблон закончил

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+console.log(
+  '🚀 ~ file: index.js ~ line 1 ~ markupRecipeTmp(arrayRecipe)',
+  markupRecipeTmp(arrayRecipe),
+);
 import '../src/sass/main.scss';
 import arrayRecipe from '../src/templates/menu.json';
+
 import markupRecipeTmp from '../src/templates/markup-recipe.hbs';
 
 const menuRecipie = markupRecipeTmp(arrayRecipe);
+const insertMenu = document.querySelector('.js-menu');
+
+insertMenu.insertAdjacentHTML('afterbegin', menuRecipie);
