50734d978ce6fd1ab2c6baa4566abdd1a95bae43
Oleksii83 <zaaytav@gmail.com>
2021-06-11 20:18:49 +0300

    Шаблон

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import './sass/main.scss';
-import arrayRecipies from './templates/menu.json';
+import '../src/sass/main.scss';
+import arrayRecipe from '../src/templates/menu.json';
 import markupRecipeTmp from '../src/templates/markup-recipe.hbs';
+
+const menuRecipie = markupRecipeTmp(arrayRecipe);
