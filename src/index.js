71f2bf3f7213a8a9782d5fe7a47a1d3eb43d2a35
Oleksii83 <zaaytav@gmail.com>
2021-06-11 21:53:41 +0300

    Закончил переключение тем

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-console.log(
-  '🚀 ~ file: index.js ~ line 1 ~ markupRecipeTmp(arrayRecipe)',
-  markupRecipeTmp(arrayRecipe),
-);
 import '../src/sass/main.scss';
 import arrayRecipe from '../src/templates/menu.json';
 
@@ -9,5 +5,19 @@ import markupRecipeTmp from '../src/templates/markup-recipe.hbs';
 
 const menuRecipie = markupRecipeTmp(arrayRecipe);
 const insertMenu = document.querySelector('.js-menu');
-
 insertMenu.insertAdjacentHTML('afterbegin', menuRecipie);
+
+//theme sun & moon
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const bodyTheme = document.querySelector('body');
+let buttonBox = document.querySelector('.theme-switch__toggle');
+buttonBox.onclick = function () {
+  bodyTheme.classList.toggle('light-theme');
+  bodyTheme.classList.toggle('dark-theme');
+  //   bodyTheme.classList.remove('light-theme');
+  //   bodyTheme.classList.add('dark-theme');
+};
