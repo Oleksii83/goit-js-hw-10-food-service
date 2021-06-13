f07c8f568471f6aa52dd3273b51508ef76ce7d28
Oleksii83 <zaaytav@gmail.com>
2021-06-13 17:19:27 +0300

    End theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import '../src/sass/main.scss';
 import arrayRecipe from '../src/templates/menu.json';
-
 import markupRecipeTmp from '../src/templates/markup-recipe.hbs';
 
 const menuRecipie = markupRecipeTmp(arrayRecipe);
 const insertMenu = document.querySelector('.js-menu');
 insertMenu.insertAdjacentHTML('afterbegin', menuRecipie);
+const bodyTheme = document.querySelector('body');
 
 //theme sun & moon
 
@@ -13,11 +13,44 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-const bodyTheme = document.querySelector('body');
-let buttonBox = document.querySelector('.theme-switch__toggle');
-buttonBox.onclick = function () {
-  bodyTheme.classList.toggle('light-theme');
-  bodyTheme.classList.toggle('dark-theme');
-  //   bodyTheme.classList.remove('light-theme');
-  //   bodyTheme.classList.add('dark-theme');
-};
+
+// buttonBox.onclick = function () {
+//   bodyTheme.classList.toggle('light-theme');
+//   localStorage.setItem('Theme', Theme.LIGHT);
+
+//   bodyTheme.classList.toggle('dark-theme');
+//   localStorage.setItem('Theme', Theme.DARK);
+// };
+
+function onCheckbox(evt) {
+  if (evt.target.checked) {
+    bodyTheme.classList.add(Theme.DARK);
+    bodyTheme.classList.remove(Theme.LIGHT);
+
+    localStorage.setItem('Theme', Theme.DARK);
+  } else if (!evt.target.checked) {
+    bodyTheme.classList.add(Theme.LIGHT);
+    bodyTheme.classList.remove(Theme.DARK);
+
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+}
+
+const menuItem = markupRecipeTmp(arrayRecipe);
+const menuRender = document.querySelector('.js-menu');
+const buttonBox = document.querySelector('#theme-switch-toggle');
+
+savedThemeLoad();
+
+menuRender.insertAdjacentHTML('afterbegin', menuItem);
+buttonBox.addEventListener('change', onCheckbox);
+
+function savedThemeLoad() {
+  const savedTheme = localStorage.getItem('Theme');
+  if (savedTheme) {
+    bodyTheme.classList.add(savedTheme);
+    if (savedTheme === 'dark-theme') {
+      buttonBox.checked = true;
+    }
+  }
+}
